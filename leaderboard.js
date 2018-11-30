aa4d6151d66c94654fe7aa548e0a02698a9553d2
Luke Moll <luke.moll99@gmail.com>
2018-11-30 17:28:44 +0000

    Fixed leaderboard formatting

--- leaderboard.js
+++ leaderboard.js
@@ -9,28 +9,38 @@ const options = JSON.parse(fs.readFileSync("secrets.json"));
 
 const cookieJar = request.jar();
 cookieJar.setCookie(request.cookie(`session=${options.session_cookie}`), "http://adventofcode.com");
-const leaderboardURL = `https://adventofcode.com/2017/leaderboard/private/view/${options.private_leaderboard}.json`;
+const year = 2017;
+const leaderboardURL = `https://adventofcode.com/${year}/leaderboard/private/view/${options.private_leaderboard}.json`;
 
 router.post('/', formparser, (req,res) => {
     let responseURL = req.body.response_url;
-
+    // console.log('/aoc');
     
     getLeaderboard((lerr, lres, lbody) => {
+        // console.log(' Got leaderboard');
         let options = {
             uri: responseURL,
             method: 'POST',
             json: {
-                "text": formatLeaderboard(JSON.parse(lbody))
+                "author_name": "Advent of Code",
+                "author_icon": "https://runciman.hacksoc.org/~ldm/img/aoc.png",
+                "author_link": "https://adventofcode.com",
+                "text": `*Advent of Code Leaderboard ${year}*\n` + formatLeaderboard(JSON.parse(lbody))
             }
         };
-        request(options, (rerr, rres, rbody) => {
-            if(rerr) {
-                console.log(rerr);
-            }
-        });
+        // console.log(" " + options.json.text);
+        // console.log(' Got options, sending response...');
+        res.status(200).json(options.json);
+        // request(options, (rerr, rres, rbody) => {
+        //     if(rerr) {
+        //         console.log(rerr);
+        //     }
+        //     console.log(' Response sent');
+        // });
     });
 
-    res.status(200).send();
+    // res.status(200).send();
+    // console.log(' 200 sent');
 });
 
 router.get('/', (req,res) => {
@@ -47,12 +57,24 @@ function getLeaderboard(callback) {
 }
 
 function formatLeaderboard(leaderboard) {
-    leaderboard.members
-        .sort(multiFactorPredicate("local_score", "stars", "global_score"))
+    let members = Object.values(leaderboard.members);
+    return members
+        // .sort(multiFactorPredicate("local_score", "stars", "global_score"))
+        .filter(m=>m.local_score > 0)
+        .sort(sortByLocalScore)
         .map((m,i,a) => {
-            return `${i+1}) **${m.name}** ${m.stars}⨉:star: ${m.local_score}` 
-                    + m.global_score > 0? ` (global score: ${m.global_score}`:'';
+            return `${i+1}) ${m.stars}⨉:star: [${m.local_score}] *${m.name}*` 
+                   // + m.global_score > 0? ` (global score: ${m.global_score}`:'';
         }).join('\n');
+}
+
+function sortByLocalScore(a,b) {
+    if(a.local_score == b.local_score) {
+        return 0;
+    }
+    else {
+        return a.local_score > b.local_score ? -1 : 1;
+    }
 }
 
 function multiFactorPredicate(...keys) {
